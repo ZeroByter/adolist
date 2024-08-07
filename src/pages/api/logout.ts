73281022a588b8b2e1c8930c7d5ef447802f344b
Rom Levtov <romlev@rafael.co.il>
2024-08-07 10:43:09 +0300

    cleaned code

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,7 +1,5 @@
 import { NextApiResponseServerIO } from "@/types/next";
 import type { NextApiRequest } from "next";
-import UsersSQL from "@/serverlib/sql-classes/users";
-import { setLoginSession } from "@/serverlib/auth";
 import { ApiResponse } from "@/types/apiResponse";
 import { removeTokenCookie } from "@/serverlib/authCookies";
 
